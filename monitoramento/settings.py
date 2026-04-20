65424e6a4d3492cb8aa0bca2e0b9f243626ce538
newton cerezini <newton.cerezini@seplag.local>
2026-04-20 11:27:19 -0300

    Add SMTP timeout setting

--- monitoramento/settings.py
+++ monitoramento/settings.py
@@ -176,4 +176,6 @@ EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "true").lower() == "true"
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "false").lower() == "true"
+EMAIL_TIMEOUT = int(os.environ.get("EMAIL_TIMEOUT", "10"))
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "Sistema de Monitoramento <no-reply@monitoramento.local>")
